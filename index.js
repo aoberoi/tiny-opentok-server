8c63871507812d6038c9e7b3c9c8560971d973db
Ankur Oberoi <aoberoi@gmail.com>
2015-02-17 22:36:41 -0500

    adds ability to set session options mediaMode and location

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var opentok = require('opentok')(process.env.OPENTOK_KEY, process.env.OPENTOK_SECRET),
     http = require('http'),
-    url = require('url');
+    url = require('url'),
+    _ = require('lodash');
 
 var server = http.createServer(function(req, res) {
   var reqUrl = url.parse(req.url, true);
+  console.log('incoming request at path: ' + reqUrl.pathname);
   switch (reqUrl.pathname) {
     case '/':
       getSession(req, res);
@@ -11,16 +13,19 @@ var server = http.createServer(function(req, res) {
   }
 });
 
-var session;
-var getSession = function(req, res) {
+var sessions = [];
 
-  function respondWithSession() {
+var getSession = function(req, res) {
+  var reqUrl = url.parse(req.url, true);
+
+  function respondWithSession(session) {
     var sessionData = {
       apiKey: opentok.apiKey,
       sessionId: session.sessionId,
       token: opentok.generateToken(session.sessionId)
     };
 
+    console.log('getSession responding with session data');
     res.writeHead(200, {
       'Content-Type': 'application/json'
     });
@@ -28,18 +33,43 @@ var getSession = function(req, res) {
   }
 
   function respondWithError(err) {
+    console.error('getSession responding with error');
     res.writeHead(500);
-    res.end();
+    res.end(err.toString());
   }
 
-  if (!session) {
-    opentok.createSession(function(err, newSession) {
-      if (err) return respondWithError(err);
-      session = newSession;
-      respondWithSession();
+  function findSession(sessionProperties) {
+    return _.find(sessions, function(session) {
+      var matches = true;
+      _.each(sessionProperties, function(value, key) {
+        if (session[key] !== value) {
+          matches = false;
+        }
+      });
+      return matches;
     });
+  }
+
+  var desiredProperties = _.pick(reqUrl.query, 'mediaMode', 'location');
+  // Checks mediaMode because unlike location when its set to an invalid property it will not throw
+  if (  desiredProperties.mediaMode !== undefined &&
+       (desiredProperties.mediaMode !== 'routed' || desiredProperties.mediaMode !== 'relayed')) {
+    return respondWithError( new Error('Media Mode can only be set to \'routed\' or \'relayed\'. ' +
+                                'Cannot use ' + desiredProperties.mediaMode) );
+  }
+
+  var matchingSession = findSession(desiredProperties);
+  if (matchingSession) {
+    console.log('using existing session');
+    return respondWithSession(matchingSession);
   } else {
-    setImmediate(respondWithSession);
+    console.log('no existing session, creating one');
+    opentok.createSession(desiredProperties, function(err, newSession) {
+      if (err) return respondWithError(err);
+      sessions.push(newSession);
+      console.log('added to set of sessions', sessions);
+      return respondWithSession(newSession);
+    });
   }
 };
 
